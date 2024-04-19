a5121faaecd7bec6042e60e232060500dfc5307f
Gonzalo Belen <87663349+GonzaBelen@users.noreply.github.com>
2024-04-18 22:01:51 -0300

    Restart screen

---
+++ Boss rush/Assets/Scripts/UI/SceneController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class SceneController : MonoBehaviour
+{
+    [SerializeField] private GameObject restartScreen;
+
+    public void OpenRestartScreen()
+    {
+        restartScreen.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
